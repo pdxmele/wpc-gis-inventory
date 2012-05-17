7a478d6be4c1c85b0df6abb8c9853f1633930b7c
Mele Sax-Barnett <saxbarm@gmail.com>
2012-05-16 20:35:08 -0700

    it works! posting changes to cartodb

--- form-process.js
+++
@@ -1,5 +0,0 @@
-function processCurbForm() {
-    var number = document.curbForm.number.value;
-    var condition = document.curbForm.condition.value;
-    var slope = document.curbForm.slope.value;
-}
