d99258543cb6cd1f96a6cd5595b70e22efa95228
Mele Sax-Barnett <saxbarm@gmail.com>
2012-05-17 23:24:57 -0700

    taking out unnecessary stuff, hiding already evaluated features & adding 1 min refresh

--- mobile-base.js
+++ mobile-base.js
@@ -1,6 +1,5 @@
-// API key for http://openlayers.org. Please get your own at
-// http://bingmapsportal.com/ and use that instead.
-var apiKey = "AqTGBsziZHIJYYxgivLBf0hVdrAk9mWO5cQcb8Yux8sW5M8c8opEC2lZqKR1ZZXf";
+// My API key. Please get your own at http://bingmapsportal.com/ and use that instead.
+var apiKey = "Ao5Ew1XnxVey8Mh0jgfL32mbQN1pNLQoDv48u1r5BJrGsf8r0Bach7FYO5wTpbHl";
 
 // initialize map when page ready
 var map;
@@ -9,37 +8,20 @@ var sm = new OpenLayers.Projection("EPSG:900913");
 
 var init = function (onSelectFeatureFunction) {
 
-    var vector = new OpenLayers.Layer.Vector("Location range", {}); //must also take out vector mention below if remove
-
-    /*var curbLayer = new OpenLayers.Layer.Vector("Demo curb", {
-        styleMap: new OpenLayers.StyleMap({
-            externalGraphic: "css/img/mobile-loc.png",
-            graphicOpacity: 1.0,
-            graphicWidth: 16,
-            graphicHeight: 26,
-            graphicYOffset: -26
-        })
-    });
-
-    var curbs = getFeatures();
-    curbLayer.addFeatures(curbs);*/
+    var vector = new OpenLayers.Layer.Vector("Location range", {});
 
     var cartoDB = new OpenLayers.Layer.Vector("Carto DB", {
         projection: new OpenLayers.Projection("EPSG:4326"),
-        strategies: [new OpenLayers.Strategy.Fixed()],
+        strategies: [new OpenLayers.Strategy.BBOX(), 
+            new OpenLayers.Strategy.Refresh({interval: 60000, force: true})],
             protocol: new OpenLayers.Protocol.Script({
             url: "http://pdxmele.cartodb.com/api/v2/sql",
-            params: {q: "select * from corners",format:"geojson"},
+            params: {q: "select * from corners where evaluated is null", format:"geojson"},
             format: new OpenLayers.Format.GeoJSON({
                 ignoreExtraDims: true
             }),
             callbackKey: "callback"
-        })/*,
-        eventListeners: {
-            "featuresadded": function () {
-                this.map.zoomToExtent(this.getDataExtent());
-            }
-        }*/
+        })
     });
 
     var selectControl = new OpenLayers.Control.SelectFeature(cartoDB, {
@@ -74,7 +56,7 @@ var init = function (onSelectFeatureFunction) {
             new OpenLayers.Layer.OSM("OpenStreetMap", null, {
                 transitionEffect: 'resize'
             }),
-            new OpenLayers.Layer.XYZ("MapBox Streets",
+            /*new OpenLayers.Layer.XYZ("MapBox Streets",
                 [
                 "http://a.tiles.mapbox.com/v3/mapbox.mapbox-streets/${z}/${x}/${y}.png",
                 "http://b.tiles.mapbox.com/v3/mapbox.mapbox-streets/${z}/${x}/${y}.png",
@@ -91,7 +73,7 @@ var init = function (onSelectFeatureFunction) {
                     numZoomLevels: 17
                     }
             ),
-            /*new OpenLayers.Layer.Bing({
+            new OpenLayers.Layer.Bing({
                 key: apiKey,
                 type: "Road",
                 // custom metadata parameter to request the new map style - only useful
@@ -115,7 +97,6 @@ var init = function (onSelectFeatureFunction) {
                 transitionEffect: 'resize'
             }),
             vector,
-            //curbLayer,
             cartoDB
         ],
         center: new OpenLayers.LonLat(-13655000, 5709500),
@@ -156,48 +137,4 @@ var init = function (onSelectFeatureFunction) {
         ]);
         map.zoomToExtent(vector.getDataExtent());
     });
-
-    /*function getFeatures() {
-        var features = {
-            "type": "FeatureCollection",
-            "features": [
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [-13656000, 5704000]},
-                    "properties": {"Name": "Example point", "Type":"Curb ramp", "Status":"Not surveyed"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [790300, 6573900]},
-                    "properties": {"Name": "Marc Jansen", "Country":"Germany", "City":"Bonn"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [568600, 6817300]},
-                    "properties": {"Name": "Bart van den Eijnden", "Country":"Netherlands", "City":"Utrecht"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [-7909900, 5215100]},
-                    "properties": {"Name": "Christopher Schmidt", "Country":"United States of America", "City":"Boston"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [-937400, 5093200]},
-                    "properties": {"Name": "Jorge Gustavo Rocha", "Country":"Portugal", "City":"Braga"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [-355300, 7547800]},
-                    "properties": {"Name": "Jennie Fletcher ", "Country":"Scotland", "City":"Edinburgh"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [657068.53608487, 5712321.2472725]},
-                    "properties": {"Name": "Bruno Binet ", "Country":"France", "City":"Chambéry"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [667250.8958124, 5668048.6072737]},
-                    "properties": {"Name": "Eric Lemoine", "Country":"France", "City":"Theys"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [653518.03606319, 5721118.5122914]},
-                    "properties": {"Name": "Antoine Abt", "Country":"France", "City":"La Motte Servolex"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [657985.78042416, 5711862.6251028]},
-                    "properties": {"Name": "Pierre Giraud", "Country":"France", "City":"Chambéry"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [742941.93818208, 5861818.9477535]},
-                    "properties": {"Name": "Stéphane Brunner", "Country":"Switzerland", "City":"Paudex"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [736082.61064069, 5908165.4649505]},
-                    "properties": {"Name": "Frédéric Junod", "Country":"Switzerland", "City":"Montagny-près-Yverdon"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [771595.97057525, 5912284.7041793]},
-                    "properties": {"Name": "Cédric Moullet", "Country":"Switzerland", "City":"Payerne"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [744205.23922364, 5861277.319748]},
-                    "properties": {"Name": "Benoit Quartier", "Country":"Switzerland", "City":"Lutry"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [1717430.147101, 5954568.7127565]},
-                    "properties": {"Name": "Andreas Hocevar", "Country":"Austria", "City":"Graz"}},
-                { "type": "Feature", "geometry": {"type": "Point", "coordinates": [-12362007.067301,5729082.2365672]},
-                    "properties": {"Name": "Tim Schaub", "Country":"United States of America", "City":"Bozeman"}}
-            ]
-        };
-
-        var reader = new OpenLayers.Format.GeoJSON();
-        return reader.read(features);
-    }*/
-
 };
